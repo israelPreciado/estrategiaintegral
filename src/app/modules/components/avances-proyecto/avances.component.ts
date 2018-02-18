08f976f39a409e388117be1b60e5d15a1dcdb926
Georgina Preciado <isra_ifbb_mexican@hotmail.com>
2018-02-17 23:04:41 -0600

    agregue los componentes listado-clientes, proyectos y avances

---
+++ src/app/modules/components/avances-proyecto/avances.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avances-proyecto',
+  templateUrl: './avances.component.html',
+})
+export class AvancesComponent {
+  title = 'Avances del proyecto';
+}
---
+++ src/app/modules/components/listado-clientes/listado-clientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'listado-clientes',
+  templateUrl: './listado-clientes.component.html',
+  
+})
+export class ListadoClientes {
+  title = 'Listado clientes';
+}
